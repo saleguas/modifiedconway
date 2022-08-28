6a074449a5ab21802ff85e9b7ff0ba98c37fe0d6
Sal <salvadoraleguas@gmail.com>
2022-08-28 14:31:42 -0400

    Update core.py

--- src/core.py
+++ src/core.py
@@ -1,3 +1,4 @@
+import pygame
 # put all the computational logic stuff here
 
 class GameBoard:
@@ -7,7 +8,30 @@ class GameBoard:
     def __init__(self, size=(100, 100)):
         self.size = size
 
-    def render(self):
+
+    def startWindow(self):
+        pygame.init()
+        pygame.display.set_caption('Quick Start')
+        window_surface = pygame.display.set_mode((800, 600))
+
+        background = pygame.Surface((800, 600))
+        background.fill(pygame.Color('#000000'))
+
+        is_running = True
+
+        while is_running:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    is_running = False
+
+            window_surface.blit(background, (0, 0))
+
+            pygame.display.update()
+
+        
+
+    def render(self, cells):
         # render the actual board
         # call this method every frame to draw the updated board
         pass
@@ -30,4 +54,11 @@ class GameOfLife:
     def __init__(self, size=(100, 100), units=100):
         # initialize our game of life object
         self.gameboard = GameBoard(size)
+        self.units = units
     
+    def start(self):
+        # start the game
+        self.gameboard.startWindow()
+
+g = GameOfLife()
+g.start()
