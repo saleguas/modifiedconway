0ca34899fec4d4ce5c967eb095020635244cc6ba
Sal <salvadoraleguas@gmail.com>
2022-08-28 14:21:08 -0400

    Update core.py

--- src/core.py
+++ src/core.py
@@ -1,2 +1,17 @@
 # put all the computational logic stuff here
 
+class GameOfLife:
+    # making our game of life object
+
+    def __init__(self, boardWidth=100, boardHeight=100, units=100):
+        # initialize our game of life object
+        self.boardWidth = boardWidth
+        self.boardHeight = boardHeight
+        self.units = units
+
+    def render(self):
+        # render the actual board
+        # call this method every frame to draw the updated board
+        pass
+
+    
