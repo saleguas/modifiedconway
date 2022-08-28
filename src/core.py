e1c70245491bb29623be46575dbf055bdb96c109
Sal <salvadoraleguas@gmail.com>
2022-08-28 14:23:26 -0400

    Update core.py

--- src/core.py
+++ src/core.py
@@ -1,17 +1,19 @@
 # put all the computational logic stuff here
+class GameBoard:
+    # visual representation of the game board
 
-class GameOfLife:
-    # making our game of life object
-
-    def __init__(self, boardWidth=100, boardHeight=100, units=100):
-        # initialize our game of life object
-        self.boardWidth = boardWidth
-        self.boardHeight = boardHeight
-        self.units = units
+    def __init__(self, size=(100, 100)):
+        self.size = size
 
     def render(self):
         # render the actual board
         # call this method every frame to draw the updated board
         pass
 
+class GameOfLife:
+    # making our game of life object
+
+    def __init__(self, size=(100, 100), units=100):
+        # initialize our game of life object
+        self.gameboard = GameBoard(size)
     
