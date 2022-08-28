1be367c702eecf02972740ca6405c1a6b9fb3ea7
Sal <salvadoraleguas@gmail.com>
2022-08-28 15:13:36 -0400

    Update core.py

--- src/core.py
+++ src/core.py
@@ -37,7 +37,7 @@ class GameOfLife:
     # making our game of life object
     # this is the parent object that controls everything else
     # it has a game board and a list of cells
-    def __init__(self, size=(800, 800), units=100, FPS=60):
+    def __init__(self, size=(800, 800), units=1000, FPS=60):
         # initialize our game of life object
         self.gameboard = GameBoard(size)
         self.units = units
@@ -47,13 +47,13 @@ class GameOfLife:
     def generate_cells(self):
         # generate the cells for the game board
         # make possible_colors red, green or blue in hex
-        possible_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+        # possible_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
         cell_size = [5, 5]
         for x in range(self.units):
             # random position on the game board
             position = [random.randint(0, self.gameboard.size[0]), random.randint(0, self.gameboard.size[1])]
             # random color from the possible_colors list
-            color = random.choice(possible_colors)
+            color = (255, 255, 255)
             # create a cell object
             cell = Cell(position, color, cell_size)
             # add the cell to the list of cells
@@ -62,11 +62,13 @@ class GameOfLife:
     def update(self):
         # apply the rules of the game of life to the cells
         # this is where the logic is
-        for cell in self.cells:
-            # increment position by one
-            # randomly increase the horizontal position by 1 or -1
-            cell.position[0] += random.randint(-2, 2)
-            cell.position[1] += random.randint(-2, 2)
+        # for cell in self.cells:
+        #     # increment position by one
+        #     # randomly increase the horizontal position by 1 or -1
+        #     cell.position[0] += random.randint(-2, 2)
+        #     cell.position[1] += random.randint(-2, 2)
+        
+        pass
 
             
         
